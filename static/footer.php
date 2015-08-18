6131682e41b4570114900b21075b68e8bd0f64e4
Matthew Lam <mryanlam@outlook.com>
2015-08-17 19:46:54 -0700

    Moved footer to a static file

---
+++ static/footer.php
@@ -0,0 +1,13 @@
+<footer>
+  <p>&copy; Matt Azn and Andwizzle93, 2015</p>
+</footer>
+</div> <!-- /container -->
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
