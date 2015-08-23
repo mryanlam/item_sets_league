caa16780533484fb25d3f6778e1ce855a5c51dff
Matthew Lam <mryanlam@outlook.com>
2015-08-22 22:08:48 -0700

    Testing fix for white screen of death

--- champion.php
+++ champion.php
@@ -5,11 +5,11 @@
         $lang = $_GET['language'];
     }
     print ('<!-- Main jumbotron for a primary marketing message or call to action -->');
-    print ('<div class="jumbotron">');
-        print ('<div class="container">');
+    print ('<div class="jumbotron">'."\n");
+    print ('    <div class="container">'."\n");
             //Champion Name as header
             if (isset($_GET['champion'])) {
-                $data = json_decode('data/'.$lang."/champion/".$_GET['champion'].'json');
+                $data = json_decode('data/'.$lang."/champion"."/".$_GET['champion'].'json');
                 $name = $data['data'][$_GET['champion']]['name'];
                 $title = $data['data'][$_GET['champion']]['title'];
                 $image = $data['data'][$_GET['champion']]['image']['full'];
@@ -19,8 +19,8 @@
             } else {
                 print ('<h1>ERROR: Champion not specified</h1> <br>');
             }
-        print ('</div>');
-    print ('</div>');
+    print ('    </div>'."\n");
+    print ('</div>'."\n");
 
     include "static/footer.php";
 ?>
