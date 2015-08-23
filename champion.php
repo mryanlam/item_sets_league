07aa366ceaf2f0fadf653b9434886aeb67fc10dc
Matthew Lam <mryanlam@outlook.com>
2015-08-22 22:05:00 -0700

    fixed quotes

--- champion.php
+++ champion.php
@@ -4,12 +4,12 @@
     if (isset($_GET['language'])) {
         $lang = $_GET['language'];
     }
-    print (<!-- Main jumbotron for a primary marketing message or call to action -->);
-    print (<div class="jumbotron">);
-        print (<div class="container">);
+    print ('<!-- Main jumbotron for a primary marketing message or call to action -->');
+    print ('<div class="jumbotron">');
+        print ('<div class="container">');
             //Champion Name as header
             if (isset($_GET['champion'])) {
-                $data = json_decode('data/'.$lang.'/champion/'.$_GET['champion'].'json');
+                $data = json_decode('data/'.$lang."/champion/".$_GET['champion'].'json');
                 $name = $data['data'][$_GET['champion']]['name'];
                 $title = $data['data'][$_GET['champion']]['title'];
                 $image = $data['data'][$_GET['champion']]['image']['full'];
@@ -19,8 +19,8 @@
             } else {
                 print ('<h1>ERROR: Champion not specified</h1> <br>');
             }
-        print (</div>);
-    print (</div>);
+        print ('</div>');
+    print ('</div>');
 
     include "static/footer.php";
 ?>
