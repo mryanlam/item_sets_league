43b2437d6fd51ae3e3b75055caaab28e27ef5bef
Matthew Lam <mryanlam@outlook.com>
2015-08-23 21:05:53 -0700

    Added more guide info

--- guides/Vayne.php
+++ guides/Vayne.php
@@ -1,17 +1,47 @@
 <div class="col-md-4">
     <h2>Standard item build</h2><br>
-    <?php
-    print('<img src="img/item/3153.png" alt="Blade of the ruined king">');
-    print('<img src="img/item/3087.png" alt="Static Shiv">');
-    print('<img src="img/item/3046.png" alt="Phantom Dancer">');
-    print('<img src="img/item/3006.png" alt="Berzerker Greaves">');
-    print('<img src="img/item/3035.png" alt="Last Whisper">');
-    print('<img src="img/item/3140.png" alt="Quicksilver Sash">');
-    print('<img src="img/item/3072.png" alt="Bloodthirster">');
-    print('<img src="img/item/3153.png" alt="Blade of the ruined king">');
-    ?>
-
+    <img src="img/item/3153.png" alt="Blade of the Ruined King">
+    <img src="img/item/3087.png" alt="Static Shiv">
+    <img src="img/item/3046.png" alt="Phantom Dancer">
+    <img src="img/item/3006.png" alt="Berzerker Greaves">
+    <img src="img/item/3035.png" alt="Last Whisper">
+    <img src="img/item/3140.png" alt="Quicksilver Sash">
+    <img src="img/item/3072.png" alt="Bloodthirster">
+    <img src="img/item/3153.png" alt="Blade of the ruined king">
 </div>
 <div class="col-md-4">
-    <h2> Placeholder for some items </h2>
+    <img src="img/item/3153.png" alt="Blade of the ruined king">
+    <h2>Blade of the Ruined King</h2>
+    <hr>
+    <b>Gold Value</b><br>
+    <ul>
+        <li>25 attack damage = 900 </li>
+        <li>10% life steal = 550 </li>
+        <li>40% attack speed = 1200 </li>
+        <li>10 on-hit damage = 200 </li>
+        <li>Total Gold Value = 2850 </li>
+    </ul>
+    <hr>
+    <b>Passive Gold Value</b><br>
+    <p>8 on-hit damage (for every 100 of the target's current health) = 160 <br>
+    (Excluding the last 125 of the target's current health, because 10 on-hit damage is the minimum damage done). </p>
+    <hr>
+    <b>Gold Efficiency</b><br>
+    <ul>
+        <li>Blade of the Ruined King is 89.1% gold efficient, without its active.</li>
+        <li>Blade of the Ruined King's gold efficiency is further increased by 5% for every 100 of the target's current health left (excluding the last 125 of the target's current health, because 10 on-hit damage is the minimum damage done).</li>
+        <li>For the item to be considered 100% gold efficient, the passive and active would need to have a combined gold value of 350.</li>
+    </ul>
+</div>
+<div class="col-md-4">
+    <img src="img/item/3046.png" alt="Phantom Dancer">
+    <h2>Phantom Dancer</h2>
+    <hr>
+    <b>Gold Value</b><br>
+    <ul>
+        <li>50% attack speed = 1500 </li>
+        <li>35% critical strike chance = 1750 </li>
+        <li>5% movement speed = 198 </li>
+        <li>Total Gold Value = 3448 </li>
+    </ul>
 </div>
