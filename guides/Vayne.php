8f25c032bb6ad0039a0de295b907c3243f02c246
Matthew Lam <mryanlam@outlook.com>
2015-08-23 20:54:40 -0700

    Standard ITem Build changed to images

--- guides/Vayne.php
+++ guides/Vayne.php
@@ -1,10 +1,16 @@
 <div class="col-md-4">
     <h2>Standard item build</h2><br>
-    <p>Blade of the ruined king</p><br>
-    <p>Static shiv/phantom dancer</p><br>
-    <p>berserker grieves</p><br>
-    <p>last whisperer</p><br>
-    <p>QSS/BLOODTHIRSTER</p><br>
+    <?php
+    print('<img src="img/item/3153.png" alt="Blade of the ruined king">');
+    print('<img src="img/item/3087.png" alt="Static Shiv">');
+    print('<img src="img/item/3046.png" alt="Phantom Dancer">');
+    print('<img src="img/item/3006.png" alt="Berzerker Greaves">');
+    print('<img src="img/item/3035.png" alt="Last Whisper">');
+    print('<img src="img/item/3140.png" alt="Quicksilver Sash">');
+    print('<img src="img/item/3072.png" alt="Bloodthirster">');
+    print('<img src="img/item/3153.png" alt="Blade of the ruined king">');
+    ?>
+
 </div>
 <div class="col-md-4">
     <h2> Placeholder for some items </h2>
