3fa4b1ec61fccffc16a6b5c155107690300c8bbb
Matthew Lam <mryanlam@outlook.com>
2015-08-27 21:16:20 -0700

    Added Sivir to the front page

--- index.php
+++ index.php
@@ -57,9 +57,8 @@
                 <h2>Corki, the Daring Bombardier</h2>
             </div>
             <div class="col-md-4">
-                <h2>Heading</h2>
-                <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-                <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+                <a  href="champion.php?champion=Sivir"><img src="img/champion/Sivir.png" alt="Sivir"></a>
+                <h2>Sivir, the Battle Mistress</h2>
             </div>
         </div>
 
