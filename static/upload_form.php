4796746748f475912f635abf394c47b8f117f2a5
Matthew Lam <mryanlam@outlook.com>
2015-08-28 22:25:52 -0700

    more info on the site

--- static/upload_form.php
+++ static/upload_form.php
@@ -1,5 +1,7 @@
 <h1>Itemset Analyzer</h1>
-<p>Upload an itemset file here to learn more about the item efficiencies of each item in the itemset.  Note: Only some items are currently supported, the vast majority of which are meant for Marksmen.</p>
+<p>Upload an itemset file here to learn more about the item efficiencies of each item in the itemset.
+Note: Only some items are currently supported, the vast majority of which are meant for Marksmen.
+Additionally, checkout the champion icons below to see my reccomended builds on those champions.</p>
 <form method="post" enctype="multipart/form-data">
     <label for="file">Filename:</label><input type="file" name="file" id="file" /> <br />
     <input type="submit" name="submit" value="Submit" />
