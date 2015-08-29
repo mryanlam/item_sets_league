df7cc87932842d581a064625efc289b1877f0186
Matthew Lam <mryanlam@outlook.com>
2015-08-28 21:56:56 -0700

    removed unused function

--- index.php
+++ index.php
@@ -7,19 +7,6 @@
         <div class="container">
             <?php
 
-                function file_get_contents_curl($url) {
-                    $ch = curl_init();
-
-                    curl_setopt($ch, CURLOPT_HEADER, 0);
-                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //Set curl to return the data instead of printing it to the browser.
-                    curl_setopt($ch, CURLOPT_URL, $url);
-
-                    $data = curl_exec($ch);
-                    curl_close($ch);
-
-                    return $data;
-                }
-
                 //If there is a file, open it and interpret it's results
                 if (isset($_POST['submit'])) {
                     //Get API key and prepare url for static api
