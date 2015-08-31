7dd9f2de2c6fa82c1d1f84875ae4a1f09888b296
parkand1 <parkand1@uw.edu>
2015-08-30 21:41:17 -0700

    Update Sivir.php

--- guides/Sivir.php
+++ guides/Sivir.php
@@ -3,8 +3,7 @@
     <img src="img/item/3031.png" alt="Infinity Edge">
     <img src="img/item/1335.png" alt="Berserker's Greaves">
     <img src="img/item/3046.png" alt="Phantom Dancer">
-    <img src="img/item/3035.png" alt="Last Whisper">
-    <img src="img/item/3140.png" alt="Quicksilver Sash">
+    <img src="img/item/3035.png" alt="Last Whisper"
     <img src="img/item/3072.png" alt="Bloodthirster">
 </div>
 <div class="col-md-4">
@@ -73,7 +72,7 @@
 <div class="row">
 
 <div class="col-md-4">
-    <img src="img/item/3035.png" alt="Last Whisperer" width="42" height="42" align="left">
+    <img src="img/item/3035.png" alt="Last Whisper" width="42" height="42" align="left">
     <h2> Last Whisperer </h2>
     <hr>
     <b> Gold Value </b><br>
@@ -88,23 +87,6 @@
     <p> The percentage of armor penetration statistics must have a gold value of at least 860 to be efficient </p>
 </div>
 
-<div class="col-md-4">
-    <img src="img/item/3139.png" alt="Mecurial Scimitar" width="42" height="42" align="left">
-    <h2>Mecurial Scimitar</h2>
-    <hr>
-    <b>Gold Value</b><br>
-    <ul>
-        <li>+35 attack damage </li>
-        <li>+80 magic resistance </li>
-        <li>Active: Removes all debuffs from your champion and grants +50% movement speed for 1 second. 90 second cooldown </li>
-    </ul>
-    <hr>
-    <b>Gold Value</b><br>
-    <p>80 attack damage = 2880 , 35 magic resist = 700 </p>  <br>
-    <hr>
-    <b>Gold Efficiency</b><br>
-    <p>Mercurial Scimitar is 96.75% gold efficient.</p>
-</div>
 
 <div class="col-md-4">
     <img src="img/item/3072.png" alt="Bloodthirster" width="42" height="42" align="left">
